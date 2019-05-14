3b994c7cf38fa4c764d5a8d6b1819839446279a3
ShevchukI <ihShevchuk@gmail.com>
2019-05-15 00:53:45 +0300

    add About

--- app/src/main/java/com/peryite/journeyd3/fragments/AboutFragment.java
+++ app/src/main/java/com/peryite/journeyd3/fragments/AboutFragment.java
@@ -8,7 +8,6 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.peryite.journeyd3.R;
 
