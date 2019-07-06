cd05dad945e9cd6212b028469df3b05bf2a01c06
ShevchukI <ihShevchuk@gmail.com>
2019-07-06 11:52:25 +0300

    idea folder removed

--- app/src/main/java/com/peryite/journeyd3/MainActivity.java
+++ app/src/main/java/com/peryite/journeyd3/MainActivity.java
@@ -140,6 +140,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onDestroy() {
         super.onDestroy();
+        Log.d(LogTag.RESULT, "saveJourney ");
         saveJourney();
     }
 
